b7c77d0e7290cb371f7c39cec9079bfa58f9e80e
pathaksatyam0103-art <pathaksatyam0103@gmail.com>
2025-08-24 23:59:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
   server: {
     port: 3000,
     strictPort: true,
